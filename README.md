7b6b84eb65c5abd905b82ead57b2902ba8cae1b6
Nick Breen <nick@foobar.net.nz>
2016-03-31 17:17:55 +1300

    updated

--- README.md
+++ README.md
@@ -3,64 +3,17 @@
 
 # installation
 
-be sure this module is installed. It it not yet pip activated. It means coping the files
-to
+Copy the files to the letsencrypt python virtual environment.
+```
+cp -r letsencrypt_haproxy letsencrypt_haproxy-0.0.1.dist-info /path/to/letsencrypt/venv/lib/python2.7/site-packages/
+```
 
-/path/to/letsencrypt/venv/lib/python2.7/site-packages/letsencrypt_haproxy
-/path/to/letsencrypt/venv/lib/python2.7/site-packages/letsencrypt_haproxy-0.0.1.dist-info/
-
-afterwards, source the letsencrypt enviroment and get a certificate
+Specify the installer and optionally the path to output the combined certificates to.
 
 ```
 source venv/bin/activate
-letsencrypt --authenticator letsencrypt-haproxy:auth --installer  letsencrypt-haproxy:installer  -d domainname.com
-```
-
-
-# haproxy configuration. 
-
-
-Below is a snippet of haproxy configuration. 
-The ACL lines are important to redirect traffice to the ACME challenge 
-system. The LE-BIND and LE-REDIRECT are placeholders for where
-the lets encrypt system can configure the domainnames
-
-This because haproxy is not domainname/servername aware per default
-
-
-```
-frontend http
-    log     global
-    mode http
-    option tcplog
-    bind 0.0.0.0:80
-
-    # Handle LE requests and forward them locally
-    acl is_letsencrypt path_beg -i /.well-known/acme-challenge/
-    use_backend letsencrypt if is_letsencrypt
-
-
-    # This '## LE-BIND and ## LE-REDIRECT are magic placeholders'
-    # LE-BIND means (SNI) bind will be added here for the specified domain
-    # LE-REDIRECT means URL matching the given URL will be redirect to https://
-
-    ## LE-BIND example.com
-
-    ## LE-REDIRECT example.com
-    
-    default_backend default
-
-
-backend letsencrypt
-    log global 
-    mode http
-    server letsencrypt 127.0.0.1:8080
-
-
-backend default
-    log     global
-    mode http
-    option tcplog
-
-    server default 127.0.0.1:8000
+letsencrypt \
+  --authenticator webroot -webroot-path /var/www/html \
+  --installer letsencrypt-haproxy:installer --combined-path /certs \
+  --domains www.example.com,example.com
 ```
--- letsencrypt_haproxy/installer.py
+++ letsencrypt_haproxy/installer.py
@@ -1,17 +1,10 @@
 """HAproxy  Let's Encrypt installer plugin."""
 import os
-import sys
 import logging
-import re
-import subprocess
 
 import zope.component
 import zope.interface
 
-from subprocess import call
-from acme import challenges
-
-from letsencrypt import errors
 from letsencrypt import interfaces
 from letsencrypt.plugins import common
 
@@ -22,65 +15,33 @@ class Installer(common.Plugin):
     zope.interface.implements(interfaces.IInstaller)
     zope.interface.classProvides(interfaces.IPluginFactory)
 
-    description = "Haproxyt Installer"
+    description = "Haproxy Installer"
 
     @classmethod
     def add_parser_arguments(cls, add):
-        add("cf-distribution-id", default=os.getenv('CF_DISTRIBUTION_ID'),
-            help="CloudFront distribution id")
+        add("combined-path", default=os.path.dirname("/certs"),
+            help="Path to install the combined certificate to.")
 
     def __init__(self, *args, **kwargs):
         super(Installer, self).__init__(*args, **kwargs)
-
 
     def prepare(self):  # pylint: disable=missing-docstring,no-self-use
         pass  # pragma: no cover
 
     def more_info(self):  # pylint: disable=missing-docstring,no-self-use
-        return ("")
+        return ""
 
     def get_all_names(self):  # pylint: disable=missing-docstring,no-self-use
         pass  # pragma: no cover
 
     def deploy_cert(self, domain, cert_path, key_path, chain_path, fullchain_path):
-
-        path = os.path.dirname(cert_path)
-
-
-        # Write key, cert & chain in one file 
-        combined = open(path + "/combined.pem", "w")
-        key = open(key_path, "r")
-        combined.write(key.read())
-        key.close()
-
-        cert = open(cert_path, "r")
-        combined.write(cert.read())
-        cert.close()
-
-        chain = open(chain_path, "r")
-        combined.write(chain.read())
-        chain.close()
-
+        combined = open("%s/%s.pem" % os.path.join(self.conf("combined-path"), domain), "w")
+        # Write key, cert & chain in one file
+        for path in [key_path, cert_path, chain_path]:
+            path_file = open(path, "r")
+            combined.write(path_file.read())
+            path_file.close()
         combined.close()
-
-
-        # Read haproxy
-        hawrite = open("/tmp/haproxy.cfg", "w")
-        compare = "## LE-BIND " + domain
-        with open('/etc/haproxy/haproxy.cfg') as haread:
-            for line in haread:
-                if (re.match(".*## LE-BIND " + domain, line)):
-                    hawrite.write("bind *:443 ssl crt " + path + "/combined.pem" + " ## LE-BIND " + domain + "\n")
-                else:
-                    hawrite.write(line)    
-
-        hawrite.close()
-
-	os.rename("/etc/haproxy/haproxy.cfg", "/etc/haproxy/haproxy.cfg.bak")
-	os.rename("/tmp/haproxy.cfg", "/etc/haproxy/haproxy.cfg")
-
-        pass
-
 
     def enhance(self, domain, enhancement, options=None):  # pylint: disable=missing-docstring,no-self-use
         pass  # pragma: no cover
@@ -107,5 +68,4 @@ class Installer(common.Plugin):
         pass  # pragma: no cover
 
     def restart(self):  # pylint: disable=missing-docstring,no-self-use
-        call(["/etc/init.d/haproxy", "restart"])
         pass  # pragma: no cover
