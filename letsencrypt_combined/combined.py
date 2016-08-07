ec42b0f2e32a5d6b93e0fe2746f9c3022ca43739
Nick Breen <nick@foobar.net.nz>
2016-08-07 20:00:43 +1200

    Removed commin base class

--- letsencrypt_combined/combined.py
+++ letsencrypt_combined/combined.py
@@ -12,8 +12,14 @@ from certbot.plugins import common
 
 logger = logging.getLogger(__name__)
 
-class Installer(common.Plugin):
-    """Base class with nothing actually implemented"""
+
+class CombinedInstaller(common.Plugin):
+    """Combined Certificate Installer"""
+
+    zope.interface.implements(interfaces.IInstaller)
+    zope.interface.classProvides(interfaces.IPluginFactory)
+
+    description = "Combined Certificate Installer"
 
     def more_info(self):  # pylint: disable=missing-docstring,no-self-use
         return ""
@@ -48,21 +54,10 @@ class Installer(common.Plugin):
     def restart(self):  # pylint: disable=missing-docstring,no-self-use
         pass  # pragma: no cover
 
-class CombinedInstaller(common.Plugin):
-    """Combined Certificate Installer"""
-
-    zope.interface.implements(interfaces.IInstaller)
-    zope.interface.classProvides(interfaces.IPluginFactory)
-
-    description = "Combined Certificate Installer"
-
     @classmethod
     def add_parser_arguments(cls, add):
         add("path", default=os.path.normpath("/certs/"),
             help="Path to install combined certificates to.")
-
-    # def __init__(self, *args, **kwargs):
-    #     super(CombinedInstaller, self).__init__(*args, **kwargs)
 
     def prepare(self):  # pylint: disable=missing-docstring
         path = self.conf("path")
@@ -77,19 +72,18 @@ class CombinedInstaller(common.Plugin):
         combined = open(path, "w")
         # Write key, cert & chain in one file
         for x_path in [key_path, cert_path, chain_path]:
-            if (x_path and os.path.isfile(x_path)):
+            if x_path and os.path.isfile(x_path):
                 logger.debug("Concatenating file: %s" % x_path)
                 x_file = open(x_path, "r")
                 combined.write(x_file.read())
                 x_file.close()
-            elif (x_path):
+            elif x_path:
                 raise ValueError("Must exist and be a file", x_path)
         combined.close()
         logger.info("Wrote combined file to: %s" % path)
 
 
-
-class DockercloudInstaller(CombinedInstaller):
+class DockercloudInstaller(common.Plugin):
     """Combined Certificate Dockercloud Installer"""
 
     zope.interface.implements(interfaces.IInstaller)
@@ -97,15 +91,45 @@ class DockercloudInstaller(CombinedInstaller):
 
     description = "Combined Certificate Dockercloud Installer"
 
+    def more_info(self):  # pylint: disable=missing-docstring,no-self-use
+        return ""
+
+    def get_all_names(self):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def enhance(self, domain, enhancement, options=None):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def supported_enhancements(self):  # pylint: disable=missing-docstring,no-self-use
+        return []  # pragma: no cover
+
+    def get_all_certs_keys(self):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def save(self, title=None, temporary=False):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def rollback_checkpoints(self, rollback=1):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def recovery_routine(self):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def view_config_changes(self):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def config_test(self):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
+    def restart(self):  # pylint: disable=missing-docstring,no-self-use
+        pass  # pragma: no cover
+
     @classmethod
     def add_parser_arguments(cls, add):
         add("service", default="haproxy",
             help="Service name to install combined certificates to (containers of).")
         add("envvar", default="CERTS_FOLDER",
             help="Service's environment variable name that specifies the location to install combined certificates to.")
-
-    # def __init__(self, *args, **kwargs):
-    #     super(DockercloudInstaller, self).__init__(*args, **kwargs)
 
     def prepare(self):  # pylint: disable=missing-docstring
         svcs = dockercloud.Service.list(name=self.conf("service"))
